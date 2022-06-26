dd48d98485747c097318c46dcc583c929f194039
Nitay Caspi <NitayCaspi12@gmail.com>
2022-06-26 21:59:06 +0300

    fix csrf token bug

--- messages_project/settings.py
+++ messages_project/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
 if ALLOWED_HOSTS_ENV:
     ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
+CSRF_TRUSTED_ORIGINS = ['https://herolo-messanger.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
@@ -116,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
