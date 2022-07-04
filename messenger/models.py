6fe1687b82ca864b19ef7ecca70c6442c692ead5
Nitay Caspi <NitayCaspi12@gmail.com>
2022-07-04 13:39:28 +0300

    change field type

--- messenger/models.py
+++ messenger/models.py
@@ -7,10 +7,10 @@ from django.db import models
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sender', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='receiver', on_delete=models.CASCADE)
-    subject = models.TextField(max_length=255)
+    subject = models.CharField(max_length=255)
     msg_body = models.TextField(max_length=5000)
     created_at = models.DateTimeField(auto_now_add=True)
-    read_at = models.DateTimeField(null=True)
+    read_at = models.DateTimeField(null=True, blank=True)
     read = models.BooleanField(default=False)
 
     def __str__(self):
